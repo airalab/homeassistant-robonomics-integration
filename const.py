422f67ae2d3f7267ce8a6a15e59e500adf4935f1
LoSk-p <alki.spb@mail.ru>
2022-06-01 14:26:16 +0300

    translations

--- const.py
+++ const.py
@@ -1,9 +1,13 @@
-DOMAIN = "robonomics_users"
+"""Constants for the Robonomics Control integration."""
+
+DOMAIN = "robonomics_control"
 CONF_IP = "ip"
 PLATFORMS = []
 
 CONF_REPOS = "repositories"
-CONF_SEED = "seed"
-CONF_SUB_OWNER = "sub_owner_address"
+CONF_USER_SEED = "seed"
+CONF_USER_ED = "user_ed"
+CONF_SUB_OWNER_SEED = "sub_owner_seed"
+CONF_SUB_OWNER_ED = "sub_owner_ed"
 CONF_PINATA_PUB = "pinata_pub"
 CONF_PINATA_SECRET = "pinata_secret"
