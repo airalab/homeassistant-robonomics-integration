7907eaea1fd0cdcde355fc9f1ccd36d9c31785d9
nakata5321 <makar5321@gmail.com>
2022-08-11 13:15:00 +0300

    upfate ipfs installation script

--- install_ipfs.sh
+++ install_ipfs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd /home/$USER
-wget https://dist.ipfs.io/go-ipfs/v0.12.2/go-ipfs_v0.12.2_linux-arm64.tar.gz
-tar -xvzf go-ipfs_v0.12.2_linux-arm64.tar.gz
-rm go-ipfs_v0.12.2_linux-arm64.tar.gz
+wget https://dist.ipfs.io/go-ipfs/v0.14.0/go-ipfs_v0.14.0_linux-arm64.tar.gz
+tar -xvzf go-ipfs_v0.14.0_linux-arm64.tar.gz
+rm go-ipfs_v0.14.0_linux-arm64.tar.gz
 cd go-ipfs
 sudo bash install.sh
 ipfs init
