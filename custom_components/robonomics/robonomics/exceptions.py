d12c67c93368e8208fbb317565b6d40a1f5fafc0
Alena Bugrova <54861210+LoSk-p@users.noreply.github.com>
2022-09-14 15:09:12 +0300

    Delete custom_components/robonomics/robonomics directory

--- custom_components/robonomics/robonomics/exceptions.py
+++
@@ -1,8 +0,0 @@
-"""Custom exceptions for the Robonomics integration."""
-from homeassistant.exceptions import HomeAssistantError
-
-class InvalidSubAdminSeed(HomeAssistantError):
-    """Given sub admin seed is not correct """
-
-class InvalidSubOwnerAddress(HomeAssistantError):
-    """Given subscription owner address is not correct """
